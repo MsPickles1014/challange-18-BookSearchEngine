14096b1807f456a7a17d13c9d520e109f73edf8d
Ms Pickles1014 <177317399+MsPickles1014@users.noreply.github.comgit config --global user.emailgit config --global user.email noeladnelson@gmail.com>
2025-03-18 12:36:20 -0400

    updated root json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
@@ -16,27 +11,8 @@ export default defineConfig(({ command }) => ({
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: command === 'serve',
-    minify: 'terser',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-    chunkSizeWarningLimit: 1600,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
-  },
-}));
+        changeOrigin: true
+      }
+    }
+  }
+})
